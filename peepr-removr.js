408ceb92b8f4262a33c3a90fe1b2fc0ee55d7a70
Przemyslaw Kowalczyk <szeryf@negativeiq.pl>
2016-06-09 14:02:20 +0200

    fix also dynamically added elements

--- peepr-removr.js
+++ peepr-removr.js
@@ -1,4 +1,8 @@
-var elementList = document.querySelectorAll("[data-peepr]");
-for (var i = 0; i < elementList.length; i++) {
-  elementList[i].removeAttribute('data-peepr');
+var peeprRemovr = function() {
+  var elementList = document.querySelectorAll("[data-peepr]");
+  for (var i = 0; i < elementList.length; i++) {
+    elementList[i].removeAttribute('data-peepr');
+  }
 }
+
+setInterval(peeprRemovr, 100);
