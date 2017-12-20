d30a3b0dfe56f3ac6a4c4cd84ae48bcaf1c89da1
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2017-12-20 10:50:03 +0100

    update image handling

--- image.js
+++ image.js
@@ -1,2 +1,11 @@
 var img = document.getElementById('content-image');
+if (null === img) {
+  var imgs = document.getElementsByTagName('img');
+  for (var i = 0; i < imgs.length; i++) {
+    if (imgs[i].className.match('image-page-style-image')) {
+      img = imgs[i];
+      break;
+    }
+  }
+}
 top.location = img.src;
