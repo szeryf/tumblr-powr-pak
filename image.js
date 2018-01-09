c05746a32f96a269cc7d65ff1000616aaf169340
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2018-01-09 08:31:01 +0100

    improve image page

--- image.js
+++ image.js
@@ -1,11 +1,14 @@
-var img = document.getElementById('content-image');
+var img = document.getElementById('content-image')
 if (null === img) {
-  var imgs = document.getElementsByTagName('img');
+  var imgs = document.getElementsByTagName('img')
   for (var i = 0; i < imgs.length; i++) {
-    if (imgs[i].className.match('image-page-style-image')) {
-      img = imgs[i];
-      break;
+    if (
+      imgs[i].className.match('image-page-style-image') ||
+      imgs[i].alt != 'avatar'
+    ) {
+      img = imgs[i]
+      break
     }
   }
 }
-top.location = img.src;
+top.location = img.src
