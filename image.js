87ce0299c0a17b873bcb7eb1f92a1957db0cd4a1
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2018-07-30 18:41:49 +0200

    updated image page code

--- image.js
+++ image.js
@@ -1,14 +1,2 @@
-var img = document.getElementById('content-image')
-if (null === img) {
-  var imgs = document.getElementsByTagName('img')
-  for (var i = 0; i < imgs.length; i++) {
-    if (
-      imgs[i].className.match('image-page-style-image') ||
-      imgs[i].alt != 'avatar'
-    ) {
-      img = imgs[i]
-      break
-    }
-  }
-}
-top.location = img.src
+var imgs = document.querySelectorAll('img[src$="_1280.jpg"]')
+top.location = imgs[0].src
