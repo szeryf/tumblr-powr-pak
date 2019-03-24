122c637a84785f37f2f83286df58b4608a39e111
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2019-03-24 17:56:14 +0100

    update image redirection

--- image.js
+++ image.js
@@ -1,2 +1,23 @@
-var imgs = document.querySelectorAll('img[src$="_1280.jpg"]')
-top.location = imgs[0].src
+const sizes = ['1280', '640']
+const extensions = ['jpg', 'png']
+for (var i = 0; i < sizes.length; i++) {
+  for (var j = 0; j < extensions.length; j++) {
+    const selector = `img[src$="_${sizes[i]}.${extensions[j]}"]`
+    const imgs = document.querySelectorAll(selector)
+    console.log(selector, imgs)
+    if (imgs.length) {
+      top.location = imgs[0].src
+    }
+  }
+}
+const sources = document.querySelectorAll('figure>img[srcset]')
+for (var i = 0; i < sources.length; i++) {
+  const source = sources[i]
+  console.log('source', source, source.srcset)
+  const { srcset } = sources[i]
+  const urls = srcset.match(/http\S+1280.(png|jpg)/)
+  if (urls) {
+    console.log('rediect', urls[0])
+    top.location = urls[0]
+  }
+}
