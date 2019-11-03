87766e4090e6616e14c38068507234b9236bb750
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2019-11-03 16:23:10 +0100

    fixed redirecting for pictures

--- redirect.js
+++ redirect.js
@@ -1,52 +1,47 @@
 /*global chrome*/
 // Redirect to archive: comment out or remove the following block to disable (lines 2-13)
 chrome.webNavigation.onBeforeNavigate.addListener(
-    function (details) {
-        'use strict';
-        var newurl = details.url;
-        var tumblrUrlRE = /^https?:\/\/(www.)?tumblr.com\/?$/;
+    function(details) {
+        'use strict'
+        var newurl = details.url
+        var tumblrUrlRE = /^https?:\/\/(www.)?tumblr.com\/?$/
         if (!tumblrUrlRE.test(newurl)) {
-            newurl += 'archive';
+            newurl += 'archive'
         }
         if (newurl !== details.url) {
-            chrome.tabs.update(details.tabId, {url: newurl});
+            chrome.tabs.update(details.tabId, { url: newurl })
         }
     },
-    {url: [{hostSuffix: '.tumblr.com', pathEquals: '/'}]}
-);
-
+    { url: [{ hostSuffix: '.tumblr.com', pathEquals: '/' }] }
+)
 
 // Replace images with 1280px version: comment out or remove the following block to disable (lines 17-28)
 chrome.webNavigation.onBeforeNavigate.addListener(
-    function (details) {
-        'use strict';
-        var oldurl, newurl;
-        oldurl = details.url;
-        newurl = oldurl.replace(/(\d+).(png|jpg)$/, "1280.$2");
+    function(details) {
+        'use strict'
+        var oldurl, newurl
+        oldurl = details.url
+        newurl = oldurl.replace(/_(\d+).(png|jpg)$/, '_1280.$2')
         if (newurl !== oldurl) {
-            console.log('redirecting', oldurl, newurl);
-            chrome.tabs.update(details.tabId, {url: newurl});
+            console.log('redirecting', oldurl, newurl)
+            chrome.tabs.update(details.tabId, { url: newurl })
         }
     },
-    {url: [{hostSuffix: 'media.tumblr.com'}]}
-);
-
+    { url: [{ hostSuffix: 'media.tumblr.com' }] }
+)
 
 // Bring back the tracked tags: comment out or remove the following block to disable (lines 32-45)
 chrome.webNavigation.onBeforeNavigate.addListener(
-    function (details) {
-        'use strict';
-        var oldurl = details.url;
-        var newurl = oldurl.replace(
-            /search\/(.+)\/recent/,
-            function (_, p1) {
-                return 'tagged/' + p1;
-            }
-        );
+    function(details) {
+        'use strict'
+        var oldurl = details.url
+        var newurl = oldurl.replace(/search\/(.+)\/recent/, function(_, p1) {
+            return 'tagged/' + p1
+        })
         if (newurl !== oldurl) {
-            console.log('redirecting', oldurl, newurl);
-            chrome.tabs.update(details.tabId, {url: newurl});
+            console.log('redirecting', oldurl, newurl)
+            chrome.tabs.update(details.tabId, { url: newurl })
         }
     },
-    {url: [{urlMatches: 'tumblr.com/search/.+/recent'}]}
-);
+    { url: [{ urlMatches: 'tumblr.com/search/.+/recent' }] }
+)
