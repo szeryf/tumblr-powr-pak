60d9a95a1c157a193483a074936263f1128426b0
Przemyslaw Kowalczyk <szeryf74@gmail.com>
2019-12-01 18:40:17 +0100

    included Italian translation, better support for image pages

--- image.js
+++ image.js
@@ -1,4 +1,4 @@
-const sizes = ['1280', '640']
+const sizes = ['1280', '1200', '640']
 const extensions = ['jpg', 'png']
 for (var i = 0; i < sizes.length; i++) {
   for (var j = 0; j < extensions.length; j++) {
@@ -13,19 +13,27 @@ for (var i = 0; i < sizes.length; i++) {
 
 const parseSources = sources => {
   for (var i = 0; i < sources.length; i++) {
-    const source = sources[i]
+    // const source = sources[i]
     // console.log('source', source, source.srcset, typeof source.srcset)
     const { srcset } = sources[i]
-    const urls = srcset.match(/http\S+1280.(png|jpg)/)
-    if (urls) {
-      console.log('rediect', urls[0])
-      top.location = urls[0]
-    }
-    const urls2 = srcset.match(/(http\S+.(png|jpg)) 1280w/)
-    // console.log('urls2', urls2)
-    if (urls2) {
-      console.log('rediect', urls2[1])
-      top.location = urls2[1]
+    console.log('srcset', srcset)
+    for (var j = 0; j < sizes.length; j++) {
+      const reg1 = new RegExp(`http\\S+${sizes[j]}.(png|jpg)`)
+      const urls = srcset.match(reg1)
+      console.log('reg1', reg1, urls)
+      if (urls) {
+        console.log('redirect', urls[0])
+        top.location = urls[0]
+        return
+      }
+      const reg2 = new RegExp(`(http\\S+.(png|jpg)) ${sizes[j]}w`)
+      const urls2 = srcset.match(reg2)
+      console.log('reg2', reg2, urls2)
+      if (urls2) {
+        console.log('redirect', urls2[1])
+        top.location = urls2[1]
+        return
+      }
     }
   }
 }
